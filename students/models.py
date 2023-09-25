00018588f1811045abd77b75195298c86363ba96
Nick Harmony <nickharmony72@gmail.com>
2023-09-25 19:08:01 +0300

    students

--- students/models.py
+++ students/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class Student(models.Model):
-    Student_No = models.PositiveIntegerField()
-    First_name = models.CharField(max_length=50)
-    Last_name = models.CharField(max_length=50)
-    Email = models.EmailField(max_length=100)
-    Field_of_study = models.CharField(max_length=50)
-    GPA = models.FloatField()
-    
-    def __str__(self):
-        return f'Student: {self.First_name} {self.Last_name}'
-    
-   
+  student_number = models.PositiveIntegerField()
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  email = models.EmailField(max_length=100)
+  field_of_study = models.CharField(max_length=50)
+  gpa = models.FloatField()
+
+  def __str__(self):
+    return f'Student: {self.first_name} {self.last_name}'
