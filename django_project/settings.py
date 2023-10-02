78e6d7f8350125d0f7a8350b0b9fedadf173040e
Nick Harmony <nickharmony72@gmail.com>
2023-10-02 08:43:54 +0300

    students

--- django_project/settings.py
+++ django_project/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'student',
+        'USER' : 'root',
+        'PASSWORD' : '2248',
+        'HOST' : 'localhost',
+        'PORT' : 3306,
     }
 }
 
